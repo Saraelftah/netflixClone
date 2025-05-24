60d535c5c3d39b48c47779a286404715e6761460
unknown <asara9086@gmail.com>
2025-05-24 21:05:18 +0300

    add db.json

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,10 +61,6 @@ function displayMovie(results, sectionID) {
     console.log(movieArr);
     console.log(sectionID);
 
-    // ============================================
-    // displayMovieInfo(movieArr);
-    // ===========================================
-    
     myMovieSlider(movieArr, sectionID);
 }
 
@@ -160,6 +156,60 @@ function myMovieSlider(movies, sectionID) {
 
 }
 
+function validateSignup () {
+  
+    const signupForm = document.getElementById('signup-form');
+
+    signupForm.addEventListener('submit', async function(e){
+        e.preventDefault();
+
+        const name = document.getElementById('name').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
+        const confirmPassword = document.getElementById('confirm-password').value.trim();
+        const passwordMatch = document.querySelector('.password-match');
+
+        if(password !== confirmPassword){
+            console.log('password does not match');
+            passwordMatch.style.display = 'block';
+            return
+        }
+
+        try{
+
+            const res = await fetch('http://localhost:3000/users');
+            const data = await res.json();
+            for(const user of data) {
+                if(user.email === email) {
+                    console.log('email alredy exist');
+                    return;
+                }
+             }
+        
+
+            const response = await fetch("http://localhost:3000/users", {
+
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({name, email, password})
+            });
+
+            if (response.ok) {
+                window.location.href = "signin.html";
+
+            }else{
+                console.log('Registration failed!')
+            }
+        } catch(error)
+         { console.error('Signup Error:', error);
+         }
+   
+    });  
+    
+}
+validateSignup ();
 
 
 
